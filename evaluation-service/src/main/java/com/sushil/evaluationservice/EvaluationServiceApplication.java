b3207aa09c967f996df4a6d8fac977ca5629d242
sushil1908 <skverma8348@gmail.com>
2026-04-24 00:17:12 +0530

    feat: initialize evaluation-service with base setup, entities, and database configuration

---
+++ evaluation-service/src/main/java/com/sushil/evaluationservice/EvaluationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sushil.evaluation_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationServiceApplication.class, args);
+	}
+
+}
