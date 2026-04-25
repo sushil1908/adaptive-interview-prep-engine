660764ae7701d5eea900f54b2c566319b049888e
sushil1908 <skverma8348@gmail.com>
2026-04-25 12:25:14 +0530

    feat: add bulk answer retrieval API for question-service to support evaluation-service optimization

---
+++ question-service/src/main/java/com/sushil/questionService/dto/QuestionAnswerRequest.java
@@ -0,0 +1,15 @@
+package com.sushil.questionService.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionAnswerRequest {
+
+    List<Integer> questionIds;
+}
