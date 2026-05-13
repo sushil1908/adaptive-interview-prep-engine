d745014cb692023833805e28f2f80cc200049d79
sushil1908 <skverma8348@gmail.com>
2026-05-13 16:03:39 +0530

    Added API Gateway with route configuration

---
+++ api-gateway/src/main/java/com/sushil/api_gateway/GatewayConfig.java
@@ -0,0 +1,40 @@
+package com.sushil.api_gateway;
+
+import org.springframework.cloud.gateway.server.mvc.filter.BeforeFilterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class GatewayConfig {
+
+    @Bean
+    public RouterFunction<ServerResponse> userServiceRoute() {
+        return GatewayRouterFunctions.route("user-service")
+                .route(req -> req.path().startsWith("/user/"),
+                        HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri("http://localhost:8080"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> questionServiceRoute() {
+        return GatewayRouterFunctions.route("question-service")
+                .route(req -> req.path().startsWith("/question/"),
+                        HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> evaluationServiceRoute() {
+        return GatewayRouterFunctions.route("evaluation-service")
+                .route(req -> req.path().startsWith("/evaluation/"),
+                        HandlerFunctions.http())
+                .before(BeforeFilterFunctions.uri("http://localhost:8082"))
+                .build();
+    }
+}
